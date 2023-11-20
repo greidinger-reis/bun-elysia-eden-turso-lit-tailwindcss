5146b95c82404c656b9ed50ce09ff0c75a6a311c
greidinger-reis <jp.greidinger@hotmail.com>
2023-11-20 14:54:24 -0300

    move counter fetch to server

--- src/pages/example.ts
+++ src/pages/example.ts
@@ -3,8 +3,14 @@ import { context } from '@/context'
 import Elysia from 'elysia'
 import { html } from 'lit'
 
-export const ExamplePage = new Elysia({ prefix: '/example' }).use(context).get('/', (ctx) => {
+export const ExamplePage = new Elysia({ prefix: '/example' }).use(context).get('/', async (ctx) => {
+	if(!ctx.session) return ctx.redirect('/auth/signin')
+
+	const userCounters = await ctx.db.query.counter.findMany({
+		where: ({id}, {eq})=> eq(id,ctx.session!.user.id)
+	}).then((counters) => counters.map(({count}) => count))
+
 	return ctx.render(BaseHtml(html`<div>Example Page
-			<example-component></example-component>
+			<example-component .counters=${userCounters}></example-component>
 	</div>`))
 })
--- src/web/components/example-component.ts
+++ src/web/components/example-component.ts
@@ -1,29 +1,13 @@
 import { html } from 'lit'
-import { customElement, state } from 'lit/decorators.js'
+import { customElement, property, state } from 'lit/decorators.js'
 import { LitElementWithEden } from './shared/element'
 
 @customElement('example-component')
 export class ExampleComponent extends LitElementWithEden {
-	@state()
-	private counters: number[]
+	@property({ type: Array })
+	counters: number[]
 	@state()
 	private error: string
-
-	async handleGetCounters() {
-		const { data: counters, status } = await ExampleComponent.client.api.exampleComponent.counters.get()
-		if (status === 200) {
-			this.counters = counters!.map(c => c.count)
-			return
-		}
-		this.counters = []
-
-		this.error = 'Failed to get counters, status: ' + status
-	}
-
-	connectedCallback() {
-		super.connectedCallback()
-		this.handleGetCounters()
-	}
 
 	render() {
 		return html`<div>
